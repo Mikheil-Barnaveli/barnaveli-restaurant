f426955e3e9565fd8db8ca8b0fd8a8c3dcabe8be
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-06-15 12:57:42 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -32,6 +32,30 @@ function App() {
     setIsDropDownVisible(!isDropDownVisible);
   };
 
+
+
+  // const handleChangeQuantity = (changeType) => {
+  //   if (changeType === '+') {
+  //     setMyObject(prevState => ({ ...prevState, quantity: prevState.quantity + 1 }));
+  //   } else if (changeType === '-' && myObject.quantity > 0) {
+  //     setMyObject(prevState => ({ ...prevState, quantity: prevState.quantity - 1 }));
+  //   }
+  // };
+
+
+  // const [cartData, setCartData] = useState([])
+
+  // const handleAddItem = () => {
+  //   if (myObject.quantity > 0){
+  //     setCartData(prevState => [...prevState, myObject]);
+  //   setMyObject(defaultObject)}
+  //   console.log(cartData, "es aris cart data");
+  // };
+
+//  const updatedCartData = [...myArray, newObject];
+
+//  setMyArray(updatedArray);
+
   return (
     <>
       <Header
@@ -48,7 +72,7 @@ function App() {
         <Route path="/barnaveli-restaurant" element={<Main data={data} />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Menu" element={<Menu menuData={data} />} />
-        <Route path="/Menu/:menuItem" element={<MenuItemPage data={data} />} />
+        <Route path="/Menu/:menuItem" element={<MenuItemPage data={data} handleCart={{}}/>} />
       </Routes>
 
       <Footer />
--- src/MenuItemPage/MenuItemPage.js
+++ src/MenuItemPage/MenuItemPage.js
@@ -1,40 +1,80 @@
-import React from 'react'
-import './MenuItemPage.css'
-import { useNavigate, useParams } from 'react-router-dom'
-import bgImage from '../assets/images/background-img.jpg'
+import React, { useState } from "react";
+import "./MenuItemPage.css";
+import { useNavigate, useParams } from "react-router-dom";
+import bgImage from "../assets/images/background-img.jpg";
 
 function MenuItemPage(props) {
 
-const params = useParams();
+  const params = useParams();
   let navigate = useNavigate();
 
-  console.log(params, "es aris paramsi");
 
-  console.log(props.data, "es aris propsi");
+
+  // console.log(params, "es aris paramsi");
+  // console.log(props.data, "es aris propsi");
+
 
   const element = props.data.find((el) => {
     return el.id === Number(params.menuItem);
   });
 
-  console.log(element, "es aris elementis");
+  // const [myObject, setMyObject] = useState(element);
+  // const [cartData, setCartData] = useState([]);
+  // const handleCart = () => {
 
-    
+  //   // if (myObject.quantity > 0){}
+  //   setCartData(prevState => [...prevState, myObject]);
+  //   setMyObject(element)
+  //   console.log(cartData);
+  // };
+
+  const [amountNumber, setAmountNumber] = useState(0)
+  function handlePlusAmount() {
+    setAmountNumber(prevState => prevState + 1)
+  }
+  function handleMinusAmount() {
+    if(amountNumber > 0){
+    setAmountNumber(prevState => prevState - 1)}
+  }
+
+  // console.log(element, "es aris elementis");
+
   return (
-    <div id='menu-item-page-div' >
-
-        
-
-        <div id='menu-item-page-image-div'>
-          <img src={element.image} id='menu-item-page-image'></img>
+    <div id="menu-item-page-div">
+      <div id="menu-item-page-image-div">
+        <img src={element.image} id="menu-item-page-image"></img>
+      </div>
+      <div id="menu-item-page-info-div">
+        <div id="menu-item-page-description-div">
+          <h2 className="menu-item-page-info-h2">{element.name}</h2>
+          <h2 className="menu-item-page-info-h2">
+            Price:{" "}
+            <span className="menu-item-page-info-span">{element.price}</span>$
+          </h2>
+          <h2 className="menu-item-page-info-h2">
+            Description:{" "}
+            <span className="menu-item-page-info-span">
+              {element.description}
+            </span>
+          </h2>
         </div>
-        <div id='menu-item-page-info-div'>
-          <h2>{element.name}</h2>
-          <h2>Price: <span className='menu-item-page-info-span'>{element.price}</span></h2>
-          <h2>Description: <span className='menu-item-page-info-span'>{element.description}</span></h2>
+        <div id="menu-item-page-button-div">
+          <div id="menu-item-page-amount-btns">
+            <button
+            onClick={handleMinusAmount}
+              className="quantity-btn"
+            >-</button>
+            <p className="quantity">{amountNumber}</p>
+            <button
+            onClick={handlePlusAmount}
+              className="quantity-btn"
+            >+</button>
+          </div>
+          <button className="menu-item-page-button" onClick={{}}>Add to Cart</button>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default MenuItemPage
+export default MenuItemPage;
